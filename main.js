cfff50f73de27434c3964da785261f97be98625b
M7MDFT00 <Eidzuoudmohmmad@gmail.com>
2025-03-25 15:40:20 +0300

    0.0.1

---
+++ main.js
@@ -0,0 +1,9 @@
+ let Owner=prompt('hello write number (5)');
+
+
+if (Owner >= 5) {
+    let name=prompt('write your name');
+    console.log('hello' + ' ' + name);
+    alert("To open the console, right-click on the page and select 'Inspect', then click on 'Console' from the tabs.");
+
+}
